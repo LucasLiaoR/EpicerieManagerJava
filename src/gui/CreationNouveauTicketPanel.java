3a069adc7c58cecc2164b8ec7fac059c2b409c4a
ducthinh96 <43443067+ducthinh96@users.noreply.github.com>
2020-09-08 22:52:37 +0200

    Ajout du mécanisme qui permet de switcher les écrans

---
+++ src/gui/CreationNouveauTicketPanel.java
@@ -0,0 +1,31 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
+public class CreationNouveauTicketPanel {
+	private JPanel panel;
+	
+	public CreationNouveauTicketPanel ()
+	{
+		panel = new JPanel();
+		panel.setBounds(181, 38, 851, 512);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Création d'un nouveau ticket");
+		lblNewLabel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(325, 0, 239, 48);
+		panel.add(lblNewLabel_1);
+	}
+	
+	public JPanel getPanel() {
+		return panel;
+	}
+}
---
+++ src/gui/ModificationQuantiteStockPanel.java
@@ -0,0 +1,31 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
+public class ModificationQuantiteStockPanel {
+	private JPanel panel;
+	
+	public ModificationQuantiteStockPanel ()
+	{
+		panel = new JPanel();
+		panel.setBounds(181, 38, 851, 512);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Modification de la quantité d'un produit");
+		lblNewLabel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(325, 0, 300, 48);
+		panel.add(lblNewLabel_1);
+	}
+	
+	public JPanel getPanel() {
+		return panel;
+	}
+}
