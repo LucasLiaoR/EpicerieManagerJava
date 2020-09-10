ce34aa05a319a646892542c2612d94327d853193
ducthinh96 <43443067+ducthinh96@users.noreply.github.com>
2020-09-10 12:42:55 +0200

    Procédures pour ajouter un nouvel utilisateur

---
+++ src/database/UtilisateursActions.java
@@ -0,0 +1,58 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UtilisateursActions {
+
+	public static boolean ajouterUtilisateur(String nom, String prenom, String telephone, String mail, String adresse,
+			String mdp, String caisse, int manager) {
+		try {
+			Connection cnx = DBConnection.ConnectToDatabase();
+			Statement st = cnx.createStatement();
+
+			Utilisateurs u = new Utilisateurs(nom, prenom, telephone, mail, adresse, mdp, caisse, manager);
+			String insertStatement = u.createInsertStatement();
+
+			st.execute(insertStatement);
+			cnx.close();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static List<Utilisateurs> getUtilisateurs() {
+		List<Utilisateurs> listUtilisateurs = new ArrayList<Utilisateurs>();
+
+		try {
+			Connection cnx = DBConnection.ConnectToDatabase();
+
+			Statement st = cnx.createStatement();
+
+			// Recuperer et ajouter toutes les utilisateurs
+			ResultSet resUtilisateurs = st.executeQuery("SELECT * FROM utilisateurs");
+			while (resUtilisateurs.next()) {
+				String nom = resUtilisateurs.getString("utls_nom");
+				String prenom = resUtilisateurs.getString("utls_prenom");
+				String telephone = resUtilisateurs.getString("utls_telephone");
+				String mail = resUtilisateurs.getString("utls_mail");
+				String adresse = resUtilisateurs.getString("utls_adresse");
+				String mdp = resUtilisateurs.getString("utls_mdp");
+				String caisse = resUtilisateurs.getString("utls_caisse");
+				int manager = resUtilisateurs.getInt("utls_manager");
+				Utilisateurs u = new Utilisateurs(nom, prenom, telephone, mail, adresse, mdp, caisse, manager);
+				listUtilisateurs.add(u);
+			}
+			cnx.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listUtilisateurs;
+	}
+}
--- src/gui/AjoutUtilisateurPanel.java
+++ src/gui/AjoutUtilisateurPanel.java
@@ -1,15 +1,21 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
+import database.UtilisateursActions;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
 
 public class AjoutUtilisateurPanel {
 	private JPanel panel;
@@ -19,6 +25,8 @@ public class AjoutUtilisateurPanel {
 	private JTextField textField_Email;
 	private JTextField textField_Adresse;
 	private JPasswordField passwordField;
+	private JTextField caisseTextField;
+	private JCheckBox isManagerCheckBox;
 	
 	public AjoutUtilisateurPanel ()
 	{
@@ -36,72 +44,126 @@ public class AjoutUtilisateurPanel {
 		JLabel nomLabel = new JLabel("Nom");
 		nomLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		nomLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
-		nomLabel.setBounds(40, 84, 62, 16);
+		nomLabel.setBounds(40, 64, 62, 16);
 		panel.add(nomLabel);
 		
 		nomTextField = new JTextField();
-		nomTextField.setBounds(173, 79, 588, 29);
+		nomTextField.setBounds(173, 59, 588, 29);
 		panel.add(nomTextField);
 		nomTextField.setColumns(10);
 		
 		JLabel prenomLabel = new JLabel("Pr\u00E9nom");
 		prenomLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		prenomLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
-		prenomLabel.setBounds(40, 136, 62, 16);
+		prenomLabel.setBounds(40, 116, 62, 16);
 		panel.add(prenomLabel);
 		
 		prenomTextField = new JTextField();
 		prenomTextField.setColumns(10);
-		prenomTextField.setBounds(173, 131, 588, 29);
+		prenomTextField.setBounds(173, 111, 588, 29);
 		panel.add(prenomTextField);
 		
 		JLabel telLabel = new JLabel("T\u00E9l\u00E9phone");
 		telLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		telLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
-		telLabel.setBounds(28, 186, 90, 16);
+		telLabel.setBounds(28, 166, 90, 16);
 		panel.add(telLabel);
 		
 		textFieldTelephone = new JTextField();
 		textFieldTelephone.setColumns(10);
-		textFieldTelephone.setBounds(173, 181, 588, 29);
+		textFieldTelephone.setBounds(173, 161, 588, 29);
 		panel.add(textFieldTelephone);
 		
 		JLabel lblAdresseMail = new JLabel("Adresse mail");
 		lblAdresseMail.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAdresseMail.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblAdresseMail.setBounds(28, 237, 97, 16);
+		lblAdresseMail.setBounds(28, 217, 97, 16);
 		panel.add(lblAdresseMail);
 		
 		textField_Email = new JTextField();
 		textField_Email.setColumns(10);
-		textField_Email.setBounds(173, 232, 588, 29);
+		textField_Email.setBounds(173, 212, 588, 29);
 		panel.add(textField_Email);
 		
 		JLabel lblAdresse = new JLabel("Adresse postale");
 		lblAdresse.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAdresse.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblAdresse.setBounds(30, 289, 106, 16);
+		lblAdresse.setBounds(30, 269, 106, 16);
 		panel.add(lblAdresse);
 		
 		textField_Adresse = new JTextField();
 		textField_Adresse.setColumns(10);
-		textField_Adresse.setBounds(173, 284, 588, 29);
+		textField_Adresse.setBounds(173, 264, 588, 29);
 		panel.add(textField_Adresse);
-		
-		JButton ajouterClientBtn = new JButton("Ajouter l'utilisateur");
-		ajouterClientBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
-		ajouterClientBtn.setBounds(577, 427, 246, 58);
-		panel.add(ajouterClientBtn);
 		
 		JLabel lblMotDePasse = new JLabel("Mot de passe");
 		lblMotDePasse.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMotDePasse.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblMotDePasse.setBounds(28, 338, 106, 16);
+		lblMotDePasse.setBounds(28, 318, 106, 16);
 		panel.add(lblMotDePasse);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(173, 333, 588, 29);
+		passwordField.setBounds(173, 313, 588, 29);
 		panel.add(passwordField);
+		
+		JLabel lblCaisse = new JLabel("Caisse");
+		lblCaisse.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCaisse.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblCaisse.setBounds(28, 372, 106, 16);
+		panel.add(lblCaisse);
+		
+		JLabel lblUtilisateurManager = new JLabel("Utilisateur Manager");
+		lblUtilisateurManager.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUtilisateurManager.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblUtilisateurManager.setBounds(28, 412, 135, 16);
+		panel.add(lblUtilisateurManager);
+		
+		isManagerCheckBox = new JCheckBox("");
+		isManagerCheckBox.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		isManagerCheckBox.setBounds(173, 410, 33, 23);
+		panel.add(isManagerCheckBox);
+		
+		caisseTextField = new JTextField();
+		caisseTextField.setColumns(10);
+		caisseTextField.setBounds(173, 367, 588, 29);
+		panel.add(caisseTextField);
+		
+		JButton ajouterClientBtn = new JButton("Ajouter l'utilisateur");
+		ajouterClientBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				for (Component component : panel.getComponents()) {
+					if (component.getClass().equals(JTextField.class)) {
+						JTextField jtf = (JTextField)component;
+						if(jtf.getText().isEmpty()) {
+							JOptionPane.showMessageDialog(panel, "Veuillez saisir tous les champs !", "Attention", JOptionPane.ERROR_MESSAGE);
+							return;
+						}
+				    }
+				}
+				
+				int isManager = 0;
+				if(isManagerCheckBox.isSelected()) {
+					isManager = 1;
+				}
+				if(UtilisateursActions.ajouterUtilisateur(nomTextField.getText(), prenomTextField.getText(), textFieldTelephone.getText(), textField_Email.getText(), textField_Adresse.getText(), passwordField.getText(), caisseTextField.getText(), isManager)) {
+					JOptionPane.showMessageDialog(panel, "Nouvel utlisateur ajouté avec succès !", "Succès", JOptionPane.PLAIN_MESSAGE);
+					nomTextField.setText("");
+					prenomTextField.setText("");
+					textFieldTelephone.setText("");
+					textField_Email.setText("");
+					textField_Adresse.setText("");
+					passwordField.setText("");
+					caisseTextField.setText("");
+					isManagerCheckBox.setSelected(false);
+				} else {
+					JOptionPane.showMessageDialog(panel, "Un problème survenu. Veuillez vérifier votre saisie", "Attention", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		ajouterClientBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		ajouterClientBtn.setBounds(595, 443, 246, 58);
+		panel.add(ajouterClientBtn);
 	}
 	
 	public JPanel getPanel() {
